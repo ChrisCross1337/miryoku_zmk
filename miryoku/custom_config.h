9fe9b61b171535953e79fc6769edc56fdb28fb02
ChrisCross <chrisdieck96@gmail.com>
2024-09-05 00:52:16 -0500

    taking of the mouse definintion

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,9 +1,6 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-
-#define MIRYOKU_KLUDGE_MOUSEKEYSPR
-
 #define MIRYOKU_LAYER_EXTRA \
 &kp TAB,           &kp Q,             &kp W,             &kp E,             &kp R,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
 &kp LSHFT,         &kp A,             &kp S,             &kp D,             &kp F,             &kp H,             &kp J,             &kp K,             &kp L,             &kp SEMI,           \
