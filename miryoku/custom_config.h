781a5ca5c19a7a94c557f49ef8f9557ba6e1c540
ChrisCross <60717304+ChrisCross1337@users.noreply.github.com>
2024-09-05 03:04:54 -0500

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -18,11 +18,12 @@
 &kp LCTRL,         &kp Z,             &kp X,             &kp C,             &kp V,             &kp B,            &kp N,             &kp M,             &kp COMMA,         &kp DOT,           \
 U_NP,              U_NP,              &kp LALT,          &kp SPC,           &mo U_TAP,         &kp RET,          &kp BSPC,          &kp DEL,           U_NP,              U_NP
 
+//adding Nums to the layout
 #define MIRYOKU_LAYER_TAP \
-&kp ESC,           &kp NUM_1,         &kp W,             &kp NUM_2,         &kp NUM_5,          &kp LBKT,          &kp F7,            &kp F8,            &kp F9,            &kp RBKT,         \
-&kp LSHFT,         &kp A,             &kp S,             &kp D,             &kp NUM_6,          &kp EQL,           &kp F4,            &kp F5,            &kp F6,            &kp SEMI,         \
-&kp LCTRL,         &kp NUM_3,         &kp X,             &kp NUM_4,         &kp NUM_7,          &kp BSLH,          &kp F1,            &kp F2,            &kp F3,            &kp GRAVE,        \
-U_NP,              U_NP,              &to U_BASE,        &kp SPC,           &none,              &kp MINUS,         &kp NUM_0,         &kp DOT,           U_NP,              U_NP
+&kp ESC,           &kp NUM_1,         &kp W,             &kp NUM_2,         &kp NUM_5,         &kp NUM_8,          &kp LBKT,          &kp F7,            &kp F8,            &kp F9,            \
+&kp LSHFT,         &kp A,             &kp S,             &kp D,             &kp NUM_6,         &kp NUM_9,          &kp EQL,           &kp F4,            &kp F5,            &kp F6,            \
+&kp LCTRL,         &kp NUM_3,         &kp X,             &kp NUM_4,         &kp NUM_7,         &kp NUM_0,          &kp BSLH,          &kp F1,            &kp F2,            &kp F3,            \
+U_NP,              U_NP,              &to U_BASE,        &kp SPC,           &none,             &kp MINUS,          &kp NUM_0,         &kp DOT,           U_NP,              U_NP
 
 #define MIRYOKU_LAYER_MOUSE \
 U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
@@ -37,37 +38,15 @@ U_NP,              U_NP,              U_NA,              U_NA,              U_NA
 #define MIRYOKU_KLUDGE_DOUBLETAPBOOT
 #define XXX &none
 
-#define MIRYOKU_LAYERMAPPING_EXTRA(\
-K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
-K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
-K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
-N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
+// Direct custom layer access
+#define MIRYOKU_LAYOUTMAPPING_CORNE(\
+K00, K01, K02, K03, K04,                         K05, K06, K07, K08, K09, \
+K10, K11, K12, K13, K14,                         K15, K16, K17, K18, K19, \
+K20, K21, K22, K23, K24,                         K25, K26, K27, K28, K29, \
+N30, N31, K32, K33, K34,                         K35, K36, K37, N38, N39 \
 ) \
 XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
 XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
 XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
                K32  K33  K34       K35  K36  K37
 
-#define MIRYOKU_LAYERMAPPING_TAP(\
-K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
-K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
-K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
-N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
-) \
-XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
-XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
-XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
-               K32  K33  K34       K35  K36  K37
-
-#define MIRYOKU_LAYERMAPPING_MOUSE(\
-K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
-K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
-K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
-N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
-) \
-XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
-XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
-XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
-               K32  K33  K34       K35  K36  K37
-
-#endif
